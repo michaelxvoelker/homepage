70d342b7db742e9e33e76d9b3859afecf3cb4359
Michael Völker <mver@posteo.de>
2021-03-15 22:10:40 +0100

    Show info on root page

--- apps/homepage/src/app/app.component.ts
+++ apps/homepage/src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'homepage-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  template: `<div
+    fxFill
+    fxLayout="row"
+    fxLayout.lt-md="column"
+    fxLayoutAlign="center center"
+    fxLayoutGap="25px"
+  >
+    <img
+      style="border-radius: 50%"
+      src="assets/foto.jpg"
+      alt="Profile picture"
+      width="200px"
+      height="200px"
+    />
+    <div fxLayoutAlign=" center" fxLayout="column">
+      <h1>Michael Völker</h1>
+      <div style="font-weight: lighter">Software Engineer, Team Lead</div>
+      <p fxLayoutGap="10px">
+        <a href="https://twitter.com" title="Twitter"
+          ><fa-icon [icon]="twitter" size="2x"></fa-icon
+        ></a>
+        <a href="https://github.com" title="Github">
+          <fa-icon [icon]="github" size="2x"></fa-icon
+        ></a>
+      </p>
+    </div>
+  </div>`,
 })
 export class AppComponent {
-  title = 'homepage';
+  twitter = faTwitter;
+  github = faGithub;
 }
--- apps/homepage/src/app/app.module.ts
+++ apps/homepage/src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    FontAwesomeModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
